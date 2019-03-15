2fd58c5fb4d250f6cdc59025af9a5e73673dad05
poulahany <poulahany1@gmail.com>
2019-03-15 20:48:35 +0200

    editor id done 90%

---
+++ fireabse/editor/addTables.js
@@ -0,0 +1,13 @@
+document.getElementById('editTables-addTable').addEventListener('click', addTable);
+function addTable() {
+    if (zone.length != 0) {
+        var tableName = document.getElementById('editTable-addTable-tableName').value;
+
+        var addTableRef = firebase.database().ref('zones/' + zone + '/' + resturantName + '/tables/')
+        var obj = { [tableName]: 'Available' }
+        addTableRef.update(obj);
+    }else{
+        alert('Please Enter Zone')
+    }
+
+}
--- fireabse/editor/tablesView.js
+++ fireabse/editor/tablesView.js
@@ -1,5 +1,5 @@
-var viewDiv = document.getElementById('editorView');
-var menuRef = firebase.database().ref('resturants/' + resturantName + '/menu/')
+var viewDiv = document.getElementById('tableView');
+var menuRef = firebase.database().ref('zones/' + zone +'/'+ resturantName + '/tables/')
 menuRef.on('value', snap => {
     viewDiv.innerText = JSON.stringify(snap.val(), null, 3);
     console.log(JSON.stringify(snap.val(), null, 3))
